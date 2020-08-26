ba65feedfce840370c14de6f7286b394a1014d03
Jugg1 <adeoluwaadeboye@gmail.com>
2020-08-26 17:18:40 +0100

    turned the ajax requests to use the Fetch API

--- script/script.js
+++ script/script.js
@@ -1,36 +1,30 @@
 (function () {
 
-    let iframeHTML = "snippet/iframe-snippet.html";
+  const foliumURL = "https://folium-choropleth-map.herokuapp.com";
 
-    const foliumURL = "https://folium-choropleth-map.herokuapp.com";
+  const tableUrl = "https://ng-covid-19-api.herokuapp.com/";
 
-    const tableUrl = "https://ng-covid-19-api.herokuapp.com/";
-
-    const summaryHTML = "snippet/total-snippet.html";
-
-    const rateHTML = "snippet/rate-snippet.html";
+  const loadError = function(error, selector) {
+    let html = "<div class='text-center'>";
+    html += `<p>Error Message: ${error}</p> </div>`
+    insertHtml(selector, html);
+  }
     
-    // Convenience function for inserting innerHTML for 'select'
-    const insertHtml = function (selector, html) {
+  // Convenience function for inserting innerHTML for 'select'
+  const insertHtml = function (selector, html) {
       let targetElem = document.querySelector(selector);
       targetElem.innerHTML = html;
-    };
+  };
     
-    // Show loading icon inside element identified by 'selector'.
-    const showLoading = function (selector) {
+  // Show loading icon inside element identified by 'selector'.
+  const showLoading = function (selector) {
       let html = "<div class='col-12 text-center'>";
       html += "<img src='img/ajax_loader.gif'></div>";
       insertHtml(selector, html);
-    };
+  };
 
-    // const insertProperty = function (string, propName, propValue) {
-    //     let propToReplace = "{{" + propName + "}}";
-    //     string = string
-    //       .replace(new RegExp(propToReplace, "g"), propValue);
-    //     return string;
-    // };
-
-    const loadTable = function (wholeJson) {
+  // helper function to turn API data into a table 
+  const loadTable = function (wholeJson) {
       let finalHtml = "<table class='table table-striped table-bordered table-hover text-center'>";
       finalHtml += "<thead> <tr> <th scope='col' class='tileshead'>State</th> <th scope='col' class='tilesorangehead'>Cases</th> <th scope='col' class='tilesgreenhead'>Recovered</th> <th scope='col' class='tilesredhead'>Deaths</th>  </tr> </thead> <tbody>"
       // Loop over categories
@@ -52,9 +46,10 @@
       finalHtml += "</table>";
       //console.log(finalHtml)
       insertHtml("#tabledata", finalHtml); 
-    }
+  }
 
-    const loadSummary = function(wholeJson) {
+  // helper function to turn API data into summary cards
+  const loadSummary = function(wholeJson) {
       let confirmedcases = wholeJson["summary"]["Confirmed Cases"];
       let totaldeath = wholeJson["summary"]["Death"];
       let totaldischarged = wholeJson["summary"]["Discharged Cases"];
@@ -75,125 +70,58 @@
                   </div>`
 
       insertHtml("#summary", html);
-    }
+  }
 
-    document.addEventListener("DOMContentLoaded", function (event) {
+  // helper function to turn API data into card showing the rates
+  const loadRate = function (wholeJson) {
+      const ratedeath = Math.round((Number(wholeJson["summary"]["Death"].replace(/,/g, ""))) / (Number(wholeJson["summary"]["Confirmed Cases"].replace(/,/g, ""))) * 1000) / 10;
+      const rateDischarged = Math.round((Number(wholeJson["summary"]["Discharged Cases"].replace(/,/g, ""))) / (Number(wholeJson["summary"]["Confirmed Cases"].replace(/,/g, ""))) * 1000) / 10;
 
+      let html = `<div class="col-sm-6 tilesgreenhead">
+                    <p>RECOVERY RATE</p>
+                    <p>${rateDischarged}%</p>
+                  </div>
 
+                  <div class="col-sm-6 tilesredhead">
+                    <p>DEATH RATE</p>
+                    <p>${ratedeath}%</p>
+                  </div>`;
+
+      insertHtml("#rate", html);
+  }
+
+  // helper function to help load map asynchronously
+  const loadFolium = function () {
+      let html = `<div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="${foliumURL}" allowfullscreen></iframe>
+                  </div>`
+
+      insertHtml("#folium-map", html);
+  }
+
+  document.addEventListener("DOMContentLoaded", function (event) {
+    
+    showLoading("#rate");
     showLoading("#summary");
-    // $ajaxUtils.sendGetRequest(
-    // tableUrl,
-    // buildAndShowSummaryHTML);
-
-    // function buildAndShowSummaryHTML (categories) {
-
-    //     $ajaxUtils.sendGetRequest(
-    //         summaryHTML,
-    //         function (summaryHTML) {
-    //           let summaryViewHtml =
-    //             buildSummaryViewHtml(categories,
-    //                                     summaryHTML);
-    //           insertHtml("#summary", summaryViewHtml);
-    //         },
-    //         false);
-    // }
-
-    // function buildSummaryViewHtml(categories,
-    //     summaryHTML) {
-        
-        
-    //     let html = summaryHTML;
-    //     //let confirmedcases = categories.data.totalConfirmedCases;
-    //     let confirmedcases = categories["summary"]["Confirmed Cases"];
-    //     //var activecases = categories.data.totalActiveCases;
-    //     let totaldeath = categories["summary"]["Death"];
-    //     let totaldischarged = categories["summary"]["Discharged Cases"];
-      
-    //     html = insertProperty(html, "total_cases", confirmedcases);
-    //     html = insertProperty(html, "total_death", totaldeath);
-    //     html = insertProperty(html, "total_discharged", totaldischarged);
-
-    //     return html
-
-    //     }
-
-    
-        showLoading("#rate");
-        // $ajaxUtils.sendGetRequest(
-        // tableUrl,
-        // buildAndShowRateHTML);
-    
-        // function buildAndShowRateHTML (categories) {
-    
-        //     $ajaxUtils.sendGetRequest(
-        //         rateHTML,
-        //         function (rateHTML) {
-        //           let rateViewHtml =
-        //             buildRateViewHtml(categories,
-        //                                     rateHTML);
-        //           insertHtml("#rate", rateViewHtml);
-        //         },
-        //         false);
-        // }
-    
-        // function buildRateViewHtml(categories,
-        //     rateHTML) {
-            
-            
-        //     let html = rateHTML;
-            
-        //     const ratedeath = Math.round((Number(categories["summary"]["Death"].replace(/,/g, ""))) / (Number(categories["summary"]["Confirmed Cases"].replace(/,/g, ""))) * 1000) / 10;
-        //     const rateDischarged = Math.round((Number(categories["summary"]["Discharged Cases"].replace(/,/g, ""))) / (Number(categories["summary"]["Confirmed Cases"].replace(/,/g, ""))) * 1000) / 10;
-            
-        //     html = insertProperty(html, "rate_death", (ratedeath+"%"));
-        //     html = insertProperty(html, "rate_discharge", (rateDischarged+"%"));
-    
-        //     return html
-    
-        //     }
-
-
-
     showLoading("#tabledata");
+    showLoading("#folium-map");
+
     fetch(tableUrl).then(function(response) {
       console.log(response.status) 
       return response.json()}).then(function(wholeJson) {
         
         loadTable(wholeJson);
         loadSummary(wholeJson);
+        loadRate(wholeJson);
+        loadFolium();
       }).catch(function(error) {
         console.log(error);
-        loadError(error, "#tabledata")
+        loadError(error, "#tabledata");
+        loadError(error, "#summary");
+        loadError(error, "#rate");
+        loadError(error, "#folium-map");
       });
    
-    showLoading("#folium-map");
-   
+    });
 
-    //   $ajaxUtils.sendGetRequest(
-    //       iframeHTML,
-    //       function (iframeHTML) {
-    //         let iframeViewHtml =
-    //           buildIframeViewHtml(
-    //                                   iframeHTML);
-    //         insertHtml("#folium-map", iframeViewHtml);
-    //       },
-    //       false);
-    
-
-    // function buildIframeViewHtml(
-    //     iframeHTML) {
-        
-        
-    //     //let html = iframeHTML;
-    //     //const site = foliumURL;
-        
-    //     iframeHTML = insertProperty(iframeHTML, "site_name", foliumURL);
-    //     return iframeHTML;
-    // }
-
-
-
-});
-
-
-})();
+ })();
